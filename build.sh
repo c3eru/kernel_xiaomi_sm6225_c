c84f536ae989d0922843a543ea0fc01dac5788bf
mobxprjkt <96016081+c3eru@users.noreply.github.com>
2023-10-08 19:41:45 +0700

    build.sh: Update build script

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-# Compile script for uvite Kernel
+# Compile script for custom Kernel
 # Copyright (C) 2020-2021 Adithya R.
 
 SECONDS=0 # builtin bash timer
-ZIPNAME="uvite-$(date '+%Y%m%d-%H%M').zip"
+ZIPNAME="RN11-$(date '+%Y%m%d-%H%M').zip"
 TC_DIR="$(pwd)/tc/clang-r450784e"
 AK3_DIR="$(pwd)/android/AnyKernel3"
 DEFCONFIG="vendor/spes-perf_defconfig"
@@ -15,6 +15,8 @@ if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
 fi
 
 export PATH="$TC_DIR/bin:$PATH"
+export KBUILD_BUILD_USER=builder
+export KBUILD_BUILD_HOST=mobx-ci-task-miui
 
 if ! [ -d "$TC_DIR" ]; then
 	echo "AOSP clang not found! Cloning to $TC_DIR..."
@@ -55,7 +57,7 @@ if [ -f "$kernel" ]; then
 	echo -e "\nKernel compiled succesfully! Zipping up...\n"
 	if [ -d "$AK3_DIR" ]; then
 		cp -r $AK3_DIR AnyKernel3
-	elif ! git clone -q https://github.com/CHRISL7/AnyKernel3 -b master; then
+	elif ! git clone -q https://github.com/c3eru/AnyKernel3_spes -b master AnyKernel3; then
 		echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
 		exit 1
 	fi
